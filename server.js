98c0042fe4a82617a25f58cd4884b88d63097aed
baronoflulz <baronoflulz@gmail.com>
2021-11-08 20:42:47 +0200

    connected to mongoDB

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const connectDB = require('./config/db')
+
 
 const app = express(); 
 
+connectDB(); 
+
 app.get('/', (req, res) => res.json({msg:"Welcome to Bug Tracker API"}));
+
+
 
 const PORT = process.env.PORT || 5000;
 
