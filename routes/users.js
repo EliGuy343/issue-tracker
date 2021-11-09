02d45d2d7e581d7718da01b4294a77a4986109e3
baronoflulz <baronoflulz@gmail.com>
2021-11-09 14:01:55 +0200

    user registration

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const {check ,validationResult } = require('express-validator');
-
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 
 const User = require('../models/User');
@@ -15,12 +15,42 @@ router.post('/',[
     check('name', 'Name is required').not().isEmpty(),
     check('email','Please include a valid email').isEmail(),
     check('password', 'Please enter a password with at least 6 letters').isLength({ min:6 })   
-], (req, res) => {
+], async (req, res) => {
+
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(400).json({errors: errors.array()});
     }
-    res.send('passed');
+    const { name, email, password  } = req.body; 
+    try {
+
+        let user = await User.findOne({email});
+    
+        if(user) {
+            return res.status(400).json({msg: "user already exists"});
+        }
+        
+        user = new User({
+            name, 
+            email,
+            password,
+            date:Date.now()
+        })
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        res.send('user saved');
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
+
+    
     
 })
 
