9efeef0d19a14e8b5bda6afe54c395ed61359fa8
baronoflulz <baronoflulz@gmail.com>
2021-11-10 20:08:08 +0200

    Added Admin registeration

--- models/User.js
+++ models/User.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const UserSchema = mongoose.Schema({
+    admin:{
+        type:Boolean,
+        default:false
+    },
     name: {
         type:String,
         required: true
--- routes/users.js
+++ routes/users.js
@@ -34,6 +34,64 @@ router.post('/',[
         }
         
         user = new User({
+            admin:false,
+            name, 
+            email,
+            password,
+            date:Date.now()
+        })
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+        
+        
+            jwt.sign(payload, config.get('jwtSecret'), {
+                expiresIn:36000,
+            }, 
+            (error,token) => {
+                if(error) {
+                    throw error;
+                }
+                res.json({token});
+            }); 
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");
+    }
+    
+})
+
+router.post('/admin',[
+    check('name', 'Name is required').not().isEmpty(),
+    check('email','Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with at least 6 letters').isLength({ min:6 })   
+], async (req, res) => {
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+    const { name, email, password  } = req.body; 
+    try {
+
+        let user = await User.findOne({email});
+    
+        if(user) {
+            return res.status(400).json({msg: "user already exists"});
+        }
+        
+        user = new User({
+            admin:true,
             name, 
             email,
             password,
