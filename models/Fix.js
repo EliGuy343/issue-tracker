e1562cfadeb906c46e603de5143af2d9aafadf34
baronoflulz <baronoflulz@gmail.com>
2021-11-16 18:27:27 +0200

    Fix Schema for issue solutions

---
+++ models/Fix.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const IssueSchema = mongoose.Schema({
+    user: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    userName:{
+        type:String,
+        ref:'users'
+    },
+    issue: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'issues'
+    },
+    date: {
+        type: Date, 
+        defualt: Date.now
+    },
+
+})
+
+
+module.exports = mongoose.model('issue', IssueSchema);
