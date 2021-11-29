eb68f713adef61d06341af9a45f4cd3a17255157
baronoflulz <baronoflulz@gmail.com>
2021-11-30 01:04:23 +0200

    Router created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 import Navbar from './components/layout/Navbar'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { Fragment } from 'react';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
 const App = () => {
   return (
-    <div className="App">
+    <Router>
+    <Fragment className="container">
       <Navbar />
-    </div>
+        <div>
+          <Routes>
+            <Route exact path='/' element={<Home/>}/>
+            <Route exact path='/about' element={<About/>}/>
+          </Routes>
+        </div>
+    </Fragment>
+    </Router>
   );
 }
 
