3fc680e40d78eae087a1aa42b0ad88226cca226c
baronoflulz <baronoflulz@gmail.com>
2021-12-05 01:56:50 +0200

    added issues to allIssues page

--- client/src/components/pages/AllIssues.js
+++ client/src/components/pages/AllIssues.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import Issues from '../issues/Issues'
 const AllIssues = () => {
     return (
         <div>
-            
+              <Issues/>
         </div>
     )
 }
