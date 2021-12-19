05d245add2d7142964fe8738223b8b8110a34be0
baronoflulz <baronoflulz@gmail.com>
2021-12-19 22:02:13 +0200

    made alert context

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,8 @@ import AllIssues from './components/pages/AllIssues';
 import MyIssues from './components/pages/MyIssues';
 import IssueState from './context/issueContext/IssueState';
 import FixState from './context/fixContext/FixState';
-import AuthState from './context/authContext/AuthState'
+import AuthState from './context/authContext/AuthState';
+import AlertState from './context/alertContext/AlertState'
 import Register from './components/auth/Register';
 import Login from './components/auth/login';
 const App = () => {
@@ -16,21 +17,23 @@ const App = () => {
     <AuthState>
       <FixState>
         <IssueState>
-          <Router>
-          <Fragment>
-            <Navbar />
-              <div>
-                <Routes>
-                  <Route exact path='/' element={<Home/>}/>
-                  <Route exact path='/about' element={<About/>}/>
-                  <Route exact path='/myissues' element={<MyIssues/>}/>
-                  <Route exact path='/AllIssues' element={<AllIssues/>}/>
-                  <Route exact path='/register' element={<Register/>}/>
-                  <Route exact path='/login' element={<Login/>}/>
-                </Routes>
-              </div>
-          </Fragment>
-          </Router>
+          <AlertState>
+            <Router>
+            <Fragment>
+              <Navbar />
+                <div>
+                  <Routes>
+                    <Route exact path='/' element={<Home/>}/>
+                    <Route exact path='/about' element={<About/>}/>
+                    <Route exact path='/myissues' element={<MyIssues/>}/>
+                    <Route exact path='/AllIssues' element={<AllIssues/>}/>
+                    <Route exact path='/register' element={<Register/>}/>
+                    <Route exact path='/login' element={<Login/>}/>
+                  </Routes>
+                </div>
+            </Fragment>
+            </Router>
+          </AlertState>
         </IssueState>
       </FixState>
     </AuthState>
