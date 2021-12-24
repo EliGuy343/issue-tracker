2af3d2046d2e6d5dd5283c5ef522d7a79fb27039
LM343 <baronoflulz@gmail.com>
2021-12-24 21:35:59 +0200

    rebuilt navbar for login

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React,{Fragment, useContext} from 'react';
 import {AiFillBug} from 'react-icons/ai'; 
 import { Link } from 'react-router-dom';
-
+import AuthContext from '../../context/authContext/authContext';
+import exitLogo from '../../icons/exit.png';
 const Navbar = () => {
+    const authContext = useContext(AuthContext); 
+    const {isAuthenticated, logout, user } = authContext; 
 
-    return (
-        <div className='navbar bg-primary'>
-            <h1> <AiFillBug size={25 }/> Issue Tracker</h1>
-            <ul>
+    const authlinks = (
+        <Fragment>
+            <li>Hello { user && user.name}</li>
+            <li>
+                <a href='#!'>
+                <span className='hide-sm'>Logout</span>
+                </a>
+            </li>
+            <li>
+                <Link to='/'>Home</Link>
+            </li>
+            <li>
+                <Link to='/about'>About</Link>
+            </li>
+            <li>
+                <Link to='/myissues'>My Issues</Link>
+            </li>
+            <li>
+                <Link to='/allissues'>All Issues</Link>
+            </li>
+        </Fragment>
+    )
+
+    const guestlinks = (
+        <Fragment>
                 <li>
                     <Link to='/'>Home</Link>
                 </li>
@@ -15,17 +39,19 @@ const Navbar = () => {
                     <Link to='/about'>About</Link>
                 </li>
                 <li>
-                    <Link to='/myissues'>My Issues</Link>
-                </li>
-                <li>
-                    <Link to='/allissues'>All Issues</Link>
-                </li>
-                <li>
                     <Link to='/register'>Register</Link>
                 </li>
                 <li>
                     <Link to='/login'>Login</Link>
                 </li>
+        </Fragment>
+    )
+
+    return (
+        <div className='navbar bg-primary'>
+            <h1> <AiFillBug size={25 }/> Issue Tracker</h1>
+            <ul>
+               {isAuthenticated ? authlinks : guestlinks}
             </ul>
         </div>
 
