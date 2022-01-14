61030de87517ad811eaf2812a10ccddaeec878ab
baronoflulz <baronoflulz@gmail.com>
2022-01-15 00:33:32 +0200

    added issue stats by date

--- client/src/components/pages/StatsticsPage.js
+++ client/src/components/pages/StatsticsPage.js
@@ -1,21 +1,11 @@
-import React, {useContext, useEffect} from 'react'
-import AuthContext from '../../context/authContext/authContext'
-import { useNavigate } from 'react-router-dom'
-import StatsByCategory from '../statistics/StatsByCategory'
+import React from 'react'
+import IssueStatistics from '../statistics/IssueStatistics'
 
 const StatsticsPage = () => {
-    const authContext = useContext(AuthContext); 
-    const {isAuthenticated} = authContext
-    const navigate = useNavigate(); 
-
-
-
-
 
      return (
         <div>
-             <label  style={{"marginLeft":"35px","fontSize":"20px"}}>Statistics by Cateogry:</label>
-            <StatsByCategory />
+            <IssueStatistics/>
         </div>
         )
     
---
+++ client/src/components/statistics/IssueStatistics.js
@@ -0,0 +1,129 @@
+import React, { useEffect ,useContext } from 'react';
+import FixContext from '../../context/fixContext/fixContext';
+import IssueContext from '../../context/issueContext/issueContext';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { LineChart, Line, ResponsiveContainer } from 'recharts';
+import Spinner from '../layout/Spinner';
+
+
+const IssueStatistics = () => {
+    const fixContext = useContext(FixContext);
+    const issueContext = useContext(IssueContext); 
+    const {fixes, getFixes} = fixContext;
+    const {issues, getAllIssues } = issueContext; 
+    const dataCategory = [];
+    const dataDate = [];
+    useEffect(() => {
+        getFixes(); 
+        getAllIssues(); 
+    
+    }, [])
+
+    const categoryLookup = {}
+    const dateLookup = {}
+    debugger;
+    if(issues.length === 0) {
+        return (
+            <Spinner/>
+        )
+    }
+    if(issues.length > 0) {
+
+            for(let i= 0; i < issues.length; i++) {
+        
+                if(!(issues[i].category in categoryLookup)) {
+                    categoryLookup[issues[i].category] = new Array(3).fill(0); 
+                }
+                categoryLookup[issues[i].category][0] +=1; 
+                if(issues[i]._id in fixes ){
+                    categoryLookup[issues[i].category][1] +=1;
+                } else  {
+                    categoryLookup[issues[i].category][2] +=1;
+                }
+                
+                }
+
+                for(const [key, value] of Object.entries(categoryLookup)) {
+            
+                    dataCategory.push({
+                        "name": key,
+                        "total": value[0],
+                        "solved": value[1],
+                        "unsolved": value[2]
+                    })
+            
+                
+                }
+
+                for(let i= issues.length -1; i > 0; i--) {
+        
+                    if(!(issues[i].date.split("T")[0] in dateLookup)) {
+                        dateLookup[issues[i].date.split("T")[0]] = 1; 
+                    }
+                    else{
+                        dateLookup[issues[i].date.split("T")[0]] +=1;
+                    }
+                    
+                }
+
+                for(const [key, value] of Object.entries(dateLookup)) {
+            
+                    dataDate.push({
+                        "name": key,
+                        "amount": value,
+                    })
+            
+                
+                }
+                
+
+        return (
+            <div>
+                <label  style={{"marginLeft":"35px","fontSize":"20px"}}>Issues Amount By Cateogry:</label>   
+                <BarChart style={{"marginLeft":"175px", "marginBottom":"50px"}}
+                    width={1200}
+                    height={300}
+                    data={dataCategory}
+                    margin={{
+                        top: 15,
+                        right: 0,
+                        left: 25,
+                        bottom: 0,
+                    }}
+                    >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis tickCount={3} />
+                    <Tooltip />
+                    <Bar dataKey="total" fill="#8884d8" />
+                    <Bar dataKey="solved" fill="#82ca9d" />
+                    <Bar dataKey="unsolved" fill="#f53d3d" />
+                    <br></br>
+             
+                </BarChart>
+                <label style={{"marginTop":"50px","marginLeft":"35px","fontSize":"20px"}} >Issue Amount By Date:</label>
+       
+                <LineChart style={{"marginLeft":"175px"}}
+                    width={1200}
+                    height={250}
+                    data={dataDate}
+                    margin={{
+                    top: 10,
+                    right: 30,
+                    left: 0,
+                    bottom: 0,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis  tickCount={3} />
+                    <Tooltip />
+                    <Line type="monotone" dataKey="amount" stroke="#8884d8" fill="#8884d8" />
+                </LineChart>   
+            </div>
+
+        )
+    }
+}
+
+export default IssueStatistics
