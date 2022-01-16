a6e138b9979175ea8259053e12bccf40d02eb951
baronoflulz <baronoflulz@gmail.com>
2022-01-16 13:20:37 +0200

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const connectDB = require('./config/db')
-
+const express = require('express');
+const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express(); 
 
@@ -11,10 +11,11 @@ connectDB();
 // init middleware
 app.use(express.json({extended: false}));
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
 
-app.get('/', (req, res) => res.json({msg:"Welcome to Issue Tracker API"}));
-
-
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 const PORT = process.env.PORT || 5000;
 
