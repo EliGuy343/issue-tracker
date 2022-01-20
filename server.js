cb885c9e64316c8eff19c52cc449ffefc9547200
baronoflulz <baronoflulz@gmail.com>
2022-01-20 22:50:03 +0200

    Finally managed to fix it

--- server.js
+++ server.js
@@ -11,11 +11,6 @@ connectDB();
 // init middleware
 app.use(express.json({extended: false}));
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
-}
 
 const PORT = process.env.PORT || 5000;
 
@@ -25,6 +20,11 @@ app.use('/api/issues', require('./routes/issues'));
 app.use('/api/fixes', require('./routes/fixes'));
 
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
 
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
